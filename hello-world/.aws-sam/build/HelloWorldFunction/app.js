160bb1db7459ea70ee1d12cc76d26ddc9e6044e0
Jon Newton <newt@hey.com>
2021-03-14 12:52:41 -0400

    Modified lambda and deployed successfully.
    Note: async is required on handler even though a promise is not returned and await is not used.

--- hello-world/.aws-sam/build/HelloWorldFunction/app.js
+++ hello-world/.aws-sam/build/HelloWorldFunction/app.js
@@ -1,33 +1,36 @@
+'use strict';
+
 // const axios = require('axios')
 // const url = 'http://checkip.amazonaws.com/';
-let response;
 
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
+ * @param {Object} _event - API Gateway Lambda Proxy Input Format
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
+ * @param {Object} _context
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
+ * @returns {Promise<any>} object - API Gateway Lambda Proxy Output Format
+ *
  */
-exports.lambdaHandler = async (event, context) => {
-    try {
-        // const ret = await axios(url);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-                // location: ret.data.trim()
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
-
-    return response
+exports.lambdaHandler = async (_event, _context) => {
+  console.log('event', _event);
+  console.log('context', _context);
+  try {
+    // const ret = await axios(url);
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'hello world ' + Date.now()
+        // location: ret.data.trim()
+      })
+    };
+    console.log('response', JSON.stringify(response));
+    return response;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
 };
