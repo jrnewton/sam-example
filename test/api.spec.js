4004bc62b6d7f603460e5e15914792b28a65e605
Jon Newton <newt@hey.com>
2021-03-30 18:05:48 -0400

    Some testing to try to figure out problem

--- test/api.spec.js
+++ test/api.spec.js
@@ -23,24 +23,102 @@ describe('api smoke tests with authentication', function () {
     }
   });
 
-  it('calls /ping', async function () {
+  // it('calls /hello', async function () {
+  //   try {
+  //     const endpoint = await getEndpoint();
+  //     const url = endpoint + '/hello';
+
+  //     console.log('GET', url);
+  //     console.log('Authorization', user.idToken);
+
+  //     const response = await axios.get(url, {
+  //       headers: {
+  //         Authorization: user.idToken
+  //       }
+  //     });
+
+  //     assert.notStrictEqual(response, null);
+  //     assert.strictEqual(response.status, 200);
+  //     assert.strictEqual(response.data, 'test from west');
+  //     console.log(JSON.stringify(response.data));
+  //   } catch (error) {
+  //     assert.fail('hello caught error ' + error);
+  //   }
+  // });
+
+  // it('calls /ping', async function () {
+  //   try {
+  //     const endpoint = await getEndpoint();
+  //     const url = endpoint + '/ping?accessToken=' + user.accessToken;
+
+  //     console.log('GET', url);
+  //     console.log('Authorization', user.idToken);
+
+  //     const response = await axios.get(url, {
+  //       headers: {
+  //         Authorization: user.idToken
+  //       }
+  //     });
+
+  //     assert.notStrictEqual(response, null);
+  //     assert.strictEqual(response.status, 200);
+  //     assert.strictEqual(response.data.status, 'ping okay');
+  //   } catch (error) {
+  //     assert.fail('ping caught error ' + error);
+  //   }
+  // });
+
+  // it('calls GET /archive', async function () {
+  //   try {
+  //     const endpoint = await getEndpoint();
+  //     const url = endpoint + '/archive?accessToken=' + user.accessToken;
+
+  //     console.log('GET', url);
+
+  //     const response = await axios.get(url, {
+  //       headers: {
+  //         Authorization: user.idToken
+  //       }
+  //     });
+
+  //     assert.notStrictEqual(response, null);
+  //     assert.strictEqual(response.status, 200);
+  //     console.log(response.data);
+  //     //assert.strictEqual(response.data.status, 'GET archive okay');
+  //   } catch (error) {
+  //     assert.fail('GET archive caught error ' + error);
+  //   }
+  // });
+
+  it('calls POST /archive', async function () {
     try {
       const endpoint = await getEndpoint();
-      const url = endpoint + '/ping?accessToken=' + user.accessToken;
+      const url = endpoint + '/archive?accessToken=' + user.accessToken;
 
-      console.log('GET', url);
+      console.log('POST', url);
+      console.log('Authorization', user.idToken);
 
-      const response = await axios.get(url, {
+      const response = await axios.post(url, {
         headers: {
-          Authorization: user.idToken
-        }
+          //prettier-ignore
+          'Authorization': user.idToken,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          url:
+            'https://tapedeck-sample-files.s3.us-east-2.amazonaws.com/test.m3u',
+          desc: 'Backwoods Halloween 2020'
+        })
       });
 
       assert.notStrictEqual(response, null);
+
+      console.log(JSON.stringify(response));
+
       assert.strictEqual(response.status, 200);
-      assert.strictEqual(response.data.status, 'ping okay');
+      //assert.strictEqual(response.data.status, 'GET archive okay');
     } catch (error) {
-      assert.fail('ping caught error ' + error);
+      assert.fail('POST archive caught error ' + JSON.stringify(error));
     }
   });
 
